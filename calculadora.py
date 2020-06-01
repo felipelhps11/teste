929228a437d97d8e3b47d9f3d36691c35be695ec
Felipe Alves <felipe.souza@timeguilu.com.br>
2020-06-01 09:54:14 -0300

    v.0.0.1-FA-feature-SUBTRACAO

--- calculadora.py
+++ calculadora.py
@@ -4,9 +4,13 @@ while not_stop == True:
     try:
         num_one = float(input("Digite o primeor número: "))
         num_two = float(input("Digite o primeor número: "))
-        operation = input("Qual operação que desejas\n soma ")
+        operation = input("Qual operação que desejas\n soma \n subtracao ")
 
         if operation == 'soma':
-            print(num_one + num_two if operation == 'soma' else None)
+            print(num_one + num_two)
+
+        elif operation == 'subtracao':
+            print(num_one - num_two)
+            
     except ValueError:
         print("Só aceitamos Número :) ")
