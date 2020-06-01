1c3a1faf44a05e105bb26cede92d55d45c115964
Felipe Alves <felipe.souza@timeguilu.com.br>
2020-06-01 09:56:15 -0300

    v.0.0.1-FA-feature-MULTIPLICACAO

--- calculadora.py
+++ calculadora.py
@@ -4,7 +4,7 @@ while not_stop == True:
     try:
         num_one = float(input("Digite o primeor número: "))
         num_two = float(input("Digite o primeor número: "))
-        operation = input("Qual operação que desejas\n soma \n subtracao ")
+        operation = input("Qual operação que desejas\n soma \n subtracao \n multiplicacao ")
 
         if operation == 'soma':
             print(num_one + num_two)
@@ -12,5 +12,7 @@ while not_stop == True:
         elif operation == 'subtracao':
             print(num_one - num_two)
             
+        elif operation == 'multiplicacao':
+            print(num_one * num_two)
     except ValueError:
         print("Só aceitamos Número :) ")
